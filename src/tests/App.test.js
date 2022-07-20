dc0ca81ff905ea856bfccacccaa07a59f8e1bcd5
Rafael-Alencar <rafael.13.alencar13@gmail.com>
2022-07-20 11:44:58 -0400

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import App from '../App';
+
+describe('Arquivo App:',
+  () => {
+    it('verifica se contém um conjunto fixo de links de navegação):',
+      () => {
+        render(<BrowserRouter><App /></BrowserRouter>);
+        const Home = screen.getByRole('link', { name: 'Home' });
+        const About = screen.getByRole('link', { name: 'About' });
+        const Favorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+        expect(Home).toBeInTheDocument();
+        expect(About).toBeInTheDocument();
+        expect(Favorite).toBeInTheDocument();
+      });
+  });
